3696ed0279ef8b4501fe32f04339bf537d5beccd
khairunnisa00 <83465171+khairunnisa00@users.noreply.github.com>
2021-04-30 22:56:54 +0700

    upload file arkaa
    upload

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const txtElement = ['Boongin', 'Hehe'];
+let count = 0;
+let txtIndex = 0;
+let currenTxt = '';
+let words = '';
+
+(function ngetik(){
+
+	if(count == txtElement.length){
+		count = 0;
+	}
+
+	currenTxt = txtElement[count];
+
+	words = currenTxt.slice(0, ++txtIndex);
+	document.querySelector('.efek-ngetik').textContent = words;
+
+	if(words.length == currentTxt.length){
+		count++;
+		txtIndex = 0;
+	}
+
+	setTimeout(ngetik,	500);
+
+})();
